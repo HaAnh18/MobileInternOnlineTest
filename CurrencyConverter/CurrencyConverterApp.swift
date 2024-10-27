cbc9842fee111bdd72aaf23e314ce257f2c607cb
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2024-10-27 14:20:58 +0700

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Nana on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
